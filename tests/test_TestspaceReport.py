69a63a9f5edfcb585799a9ff354bf069bd7177aa
jaschultz <jschultz8960@yahoo.com>
2017-06-02 14:55:24 -0700

    Refactor testspace report (#3)
    Update to remove dead code and use standard python package naming conventions.
    
    * add xml_file to TestspaceReport
    
    * update to pep8 naming convention
    
    * update xsd to latest

--- tests/test_TestspaceReport.py
+++ tests/test_TestspaceReport.py
@@ -4,13 +4,13 @@ import os
 from lxml import etree, objectify
 from lxml.etree import XMLSyntaxError
 
+
 def create_simple_testspace_xml(self):
-    suite_root = testspace_xml.TestspaceReport()
-    example_suite = suite_root.GetOrAddSuite('Example Suite')
-    test_case = testspace_xml.TestCase(example_suite, 'test a', 'passed')
-    example_suite.AddTestCase(test_case)
-    writer = testspace_xml.XmlWriter(suite_root)
-    writer.Write('testspace.xml')
+    testspace_report = testspace_xml.TestspaceReport()
+    example_suite = testspace_report.get_or_add_suite('Example Suite')
+    test_case = testspace_xml.TestCase('test a', 'passed')
+    example_suite.add_test_case(test_case)
+    testspace_report.xml_file('testspace.xml')
 
     xml_file = open('testspace.xml', 'r')
     self.testspace_xml_string = xml_file.read()
@@ -23,7 +23,7 @@ class TestTestspaceXml:
         create_simple_testspace_xml(self)
 
     @classmethod
-    def teardown_class(self):
+    def teardown_class(cls):
         """ teardown any state that was previously setup with a call to
         setup_class.
         """
@@ -45,9 +45,3 @@ def xml_validator(some_xml_string, xsd_file):
         # handle exception here
         return False
     return True
-
-
-
-
-
-
