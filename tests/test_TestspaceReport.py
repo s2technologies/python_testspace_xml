21d4abb637bc0e8048dc2043c809ed9461ce0066
jaschultz <jschultz8960@yahoo.com>
2017-06-02 21:13:55 -0700

    update tests

--- tests/test_TestspaceReport.py
+++ tests/test_TestspaceReport.py
@@ -8,7 +8,11 @@ from lxml.etree import XMLSyntaxError
 def create_simple_testspace_xml(self):
     testspace_report = testspace_xml.TestspaceReport()
     example_suite = testspace_report.get_or_add_suite('Example Suite')
-    test_case = testspace_xml.TestCase('test a', 'passed')
+    test_case = testspace_xml.TestCase('passing case 1', 'passed')
+    example_suite.add_test_case(test_case)
+    test_case = testspace_xml.TestCase('passing case 2', 'passed')
+    example_suite.add_test_case(test_case)
+    test_case = testspace_xml.TestCase('failing case 1', 'failed')
     example_suite.add_test_case(test_case)
     testspace_report.xml_file('testspace.xml')
 
@@ -16,6 +20,7 @@ def create_simple_testspace_xml(self):
     self.testspace_xml_string = xml_file.read()
     xml_file.close()
 
+    self.testspace_xml_root = etree.fromstring(self.testspace_xml_string)
 
 class TestTestspaceXml:
     @pytest.fixture(autouse=True)
@@ -30,7 +35,16 @@ class TestTestspaceXml:
         os.remove('testspace.xml')
 
     def test_number_testcases(self):
-        assert 'passed="1"' in self.testspace_xml_string
+        test_cases = self.testspace_xml_root.xpath("//test_suite/test_case")
+        assert len(test_cases) is 3
+
+    def test_number_passed_testcases(self):
+        test_cases = self.testspace_xml_root.xpath("//test_suite/test_case[@status='passed']")
+        assert len(test_cases) is 2
+
+    def test_number_failed_testcases(self):
+        test_cases = self.testspace_xml_root.xpath("//test_suite/test_case[@status='failed']")
+        assert len(test_cases) is 1
 
     def test_validate_xsd(self):
         assert xml_validator(self.testspace_xml_string, 'tests/report_v1.xsd')
