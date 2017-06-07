bb091d5099d237a4dacec454cfcfc076b7df6d39
jaschultz <jschultz8960@yahoo.com>
2017-06-06 18:30:54 -0700

    add annotations tests

--- tests/test_TestspaceReport.py
+++ tests/test_TestspaceReport.py
@@ -7,19 +7,24 @@ from python_testspace_xml import testspace_xml
 
 
 def create_simple_testspace_xml(self):
-    self.annotation_tuple = [('annotation example', 'description of annotation'),
-                             ('aannotation example', 'to confirm order of annotations')]
+    self.annotation_tuple = [('annotation info example', 'info', 'description of annotation'),
+                             ('aannotation error example', 'error', 'to confirm order of annotations'),
+                             ('zannotation warning example', 'warn', 'to confirm order of annotations')]
     testspace_report = testspace_xml.TestspaceReport()
     example_suite = testspace_report.get_or_add_test_suite('Example Suite')
     test_case = testspace_xml.TestCase('passing case 1', 'passed')
     for annotation in self.annotation_tuple:
-        test_case.add_text_annotation(annotation[0], description=annotation[1])
+        test_case.add_text_annotation(annotation[0], annotation[1], description=annotation[2])
     example_suite.add_test_case(test_case)
     test_case = testspace_xml.TestCase('passing case 2', 'passed')
     test_case.add_file_annotation('report_v1.xsd', file_path='tests/report_v1.xsd')
     test_case.add_link_annotation(file_path=r'\\machine/public')
+    test_case.add_info_annotation(self.annotation_tuple[0][2])
+    test_case.add_error_annotation(self.annotation_tuple[1][2])
+    test_case.add_warning_annotation(self.annotation_tuple[2][2])
     example_suite.add_test_case(test_case)
-    test_case = testspace_xml.TestCase('failing case 1', 'failed')
+    test_case = testspace_xml.TestCase('failing case 1')
+    test_case.fail('failing testcase')  # adds annotation to testcase
     example_suite.add_test_case(test_case)
     testspace_report.write_xml('testspace.xml', to_pretty=True)
 
@@ -63,7 +68,8 @@ class TestTestspaceXml:
         assert len(test_cases) is 2
 
     def test_annotation_order(self):
-        annotations = self.testspace_xml_root.xpath("//test_suite/test_case[@name='passing case 1']/annotation")
+        annotations = self.testspace_xml_root.xpath(
+            "//test_suite/test_case[@name='passing case 1']/annotation")
         for idx, annotation in enumerate(annotations):
             assert annotation.get('name') == self.annotation_tuple[idx][0]
 
