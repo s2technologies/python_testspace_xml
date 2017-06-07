99c720c6e91ea16178e9a86653155699425f2dc0
jaschultz <jschultz8960@yahoo.com>
2017-06-06 18:02:07 -0700

    add annotation test order check

--- tests/test_TestspaceReport.py
+++ tests/test_TestspaceReport.py
@@ -7,10 +7,13 @@ from python_testspace_xml import testspace_xml
 
 
 def create_simple_testspace_xml(self):
+    self.annotation_tuple = [('annotation example', 'description of annotation'),
+                             ('aannotation example', 'to confirm order of annotations')]
     testspace_report = testspace_xml.TestspaceReport()
     example_suite = testspace_report.get_or_add_test_suite('Example Suite')
     test_case = testspace_xml.TestCase('passing case 1', 'passed')
-    test_case.add_text_annotation('annotation example', description='description of annotation')
+    for annotation in self.annotation_tuple:
+        test_case.add_text_annotation(annotation[0], description=annotation[1])
     example_suite.add_test_case(test_case)
     test_case = testspace_xml.TestCase('passing case 2', 'passed')
     test_case.add_file_annotation('report_v1.xsd', file_path='tests/report_v1.xsd')
@@ -37,7 +40,7 @@ class TestTestspaceXml:
         """ teardown any state that was previously setup with a call to
         setup_class.
         """
-        os.remove('testspace.xml')
+        #os.remove('testspace.xml')
 
     def test_number_testcases(self):
         test_cases = self.testspace_xml_root.xpath("//test_suite/test_case")
@@ -58,6 +61,11 @@ class TestTestspaceXml:
     def test_number_file_annotations(self):
         test_cases = self.testspace_xml_root.xpath("//test_suite/test_case/annotation[@file]")
         assert len(test_cases) is 2
+
+    def test_annotation_order(self):
+        annotations = self.testspace_xml_root.xpath("//test_suite/test_case[@name='passing case 1']/annotation")
+        for idx, annotation in enumerate(annotations):
+            assert annotation.get('name') == self.annotation_tuple[idx][0]
 
     def test_validate_xsd(self):
         assert xml_validator(self.testspace_xml_string, 'tests/report_v1.xsd')
