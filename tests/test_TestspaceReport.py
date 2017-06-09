6086bc15160faf0d111fee86508e2ae06c4aced1
jaschultz <jschultz8960@yahoo.com>
2017-06-08 17:36:12 -0700

    remove redundant testcase from merge

--- tests/test_TestspaceReport.py
+++ tests/test_TestspaceReport.py
@@ -11,13 +11,16 @@ def create_simple_testspace_xml(self):
         ('zannotation warning example', 'warn', 'to confirm order of annotations'),
         ('annotation info example', 'info', 'description of annotation'),
         ('aannotation error example', 'error', 'to confirm order of annotations')]
+
     testspace_report = testspace_xml.TestspaceReport()
+
     example_suite = testspace_report.get_or_add_test_suite('Example Suite')
     test_case = testspace_xml.TestCase('passing case 1', 'passed')
     for annotation in self.annotation_tuple:
         test_case.add_text_annotation(
             annotation[0], level=annotation[1], description=annotation[2])
     example_suite.add_test_case(test_case)
+
     test_case = testspace_xml.TestCase('passing case 2', 'passed')
     test_case.add_file_annotation('report_v1.xsd', file_path='tests/report_v1.xsd')
     test_case.add_link_annotation(file_path=r'\\machine/public')
@@ -25,6 +28,7 @@ def create_simple_testspace_xml(self):
     test_case.add_error_annotation(self.annotation_tuple[1][2])
     test_case.add_warning_annotation(self.annotation_tuple[2][2])
     example_suite.add_test_case(test_case)
+
     test_case = testspace_xml.TestCase('failing case 1')
     test_case.fail('failing testcase')  # adds annotation to testcase
     example_suite.add_test_case(test_case)
@@ -68,12 +72,6 @@ class TestTestspaceXml:
     def test_number_file_annotations(self):
         test_cases = self.testspace_xml_root.xpath("//test_suite/test_case/annotation[@file]")
         assert len(test_cases) is 2
-
-    def test_annotation_order(self):
-        annotations = self.testspace_xml_root.xpath(
-            "//test_suite/test_case[@name='passing case 1']/annotation")
-        for idx, annotation in enumerate(annotations):
-            assert annotation.get('name') == self.annotation_tuple[idx][0]
 
     def test_annotation_order(self):
         annotations = self.testspace_xml_root.xpath(
