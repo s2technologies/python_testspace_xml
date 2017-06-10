f75fdfb2f415df7f86a68fbd240e6cdf856acf30
Jeffrey Schultz <jschultz8960@yahoo.com>
2017-06-10 13:29:54 -0700

    add tox to run pytest

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testspace_api',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='jaschultz',
+    author_email='',
+    description='',
+    install_requires=[
+        'lxml'
+        'pytest',
+        'pytest-cov',
+        'flake8',
+        'pep8-naming',
+    ]
+)
