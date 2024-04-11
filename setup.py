699bb36d09beca3640edabac9e4536e9675739da
Ivailo Petrov <5092875+ivailop@users.noreply.github.com>
2024-04-11 13:44:54 -0700

    set complient setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_testspace_xml',
-    version='',
+    version='1.2',
     packages=find_packages(include=['python_testspace_xml', 'python_testspace_xml.*']),
     url='',
     license="MIT license",
